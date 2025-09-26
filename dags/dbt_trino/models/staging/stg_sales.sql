5d84f15d7655e6bc9e1c5d2e7434c3278f6ec697
Mark Pham <minh.pham@insurify.com>
2025-09-26 14:23:08 -0400

    feat: adding dependencies to dbt models

--- dags/dbt_trino/models/staging/stg_sales.sql
+++ dags/dbt_trino/models/staging/stg_sales.sql
@@ -1,5 +1,8 @@
 {{ config(materialized='table') }}
 
+-- Force dependency on product_categories to avoid concurrency issues
+{% set _ = ref('stg_product_categories') %}
+
 SELECT 
     date_parse(order_date, '%m/%d/%Y') AS order_date,
     date_parse(stock_date, '%m/%d/%Y') AS stock_date,
